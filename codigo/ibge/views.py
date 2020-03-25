59b01b4c7c49f8e29ecaa7a5d94dbd246d383840
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2020-03-24 22:54:42 -0300

    Adiciona o código da view

--- codigo/ibge/views.py
+++ codigo/ibge/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import requests # Pacote que constrói requisições HTTP e consome respostas
+import json # Pacote que trabalha com JSONs
 
-# Create your views here.
+def index(request):
+    api = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/24/municipios"
+    requisicao = requests.get(api)
+    lista = requisicao.json()
+    dicionario = {}
+    for indice, valor in enumerate(lista):
+        dicionario[indice] = valor
+
+    contexto = {
+        "municipios": dicionario
+    }
+
+    return render(request, "aplicacao/index.html", contexto)
